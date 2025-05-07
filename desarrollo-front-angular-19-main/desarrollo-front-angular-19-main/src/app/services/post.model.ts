1c06a568751452428218571a3124412b0e604cfe
natispica777 <nipinilla@ucompensar.edu.co>
2025-05-06 22:17:58 -0500

    Delete desarrollo-front-angular-19-main/desarrollo-front-angular-19-main directory

--- desarrollo-front-angular-19-main/desarrollo-front-angular-19-main/src/app/services/post.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Post {
-    autorId: string;
-    autorNombre: string;
-    contenido: string;
-    estado: string;
-    etiquetas: string[];
-    imagenUrl: string;
-    titulo: string;
-  }  
